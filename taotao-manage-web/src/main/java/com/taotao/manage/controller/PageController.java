78aab5a382a3bd9470696ae61aa56873068e1678
yutianci <745369408@qq.com>
2017-05-31 23:13:49 +0800

    后台系统环境搭建代码提交

---
+++ taotao-manage-web/src/main/java/com/taotao/manage/controller/PageController.java
@@ -0,0 +1,21 @@
+package com.taotao.manage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by Administrator on 2017/5/31.
+ * http://localhost:9082/rest/page/login
+ * http://localhost:9082/rest/page/index
+ */
+@Controller
+@RequestMapping("page")
+public class PageController {
+
+    @RequestMapping(value="{pageName}",method = RequestMethod.GET)
+    public String toPage(@PathVariable("pageName") String pageName){
+        return pageName;
+    }
+}
